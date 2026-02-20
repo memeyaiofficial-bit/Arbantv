9a44f0890f0e67d4e88be4f28b31d1030ccfe02c
eddiemwendwaa <eddiemwendwaa@gmail.com>
2026-02-20 15:11:56 +0300

    Delete backend/analytics-backend directory

--- backend/analytics-backend/src/middlewares/error.middleware.js
+++
@@ -1,8 +0,0 @@
-export const errorHandler = (err, req, res, next) => {
-  console.error(err);
-
-  res.status(500).json({
-    message: "Something went wrong",
-    error: err.message
-  });
-};
--- backend/analytics-backend/src/middlewares/rateLimit.middleware.js
+++
@@ -1,9 +0,0 @@
-import rateLimit from "express-rate-limit";
-
-export const analyticsLimiter = rateLimit({
-  windowMs: 60 * 1000, // 1 minute
-  max: 60, // 60 requests per minute
-  message: {
-    message: "Too many requests, please slow down."
-  }
-});
